a9fea68bf141ea3cdb1ff5e85aaff44ade6536e9
Carlos Pizarro <kr105@kr105.com>
2023-03-11 23:14:11 -0300

    Make GITHUB_TOKEN optional

--- src/modules/GithubAPI.js
+++ src/modules/GithubAPI.js
@@ -2,35 +2,30 @@ const repoOwner = process.env.GITHUB_REPO_OWNER || 'kr105'
 const repoName = process.env.GITHUB_REPO_NAME || 'fulltimeforce_takehome'
 const githubToken = process.env.GITHUB_TOKEN || ''
 
-async function getCommits () {
-    const data = await fetch(
-        `https://api.github.com/repos/${repoOwner}/${repoName}/commits`,
+async function fetchGithub (url) {
+    const authHeader = {
+        Authorization: `Bearer ${githubToken}`
+    }
+
+    return fetch(
+        url,
         {
             method: 'GET',
-            headers: {
-                Authorization: `Bearer ${githubToken}`
-            },
+            headers: githubToken ? authHeader : undefined, // Include token only if present
             next: {
                 revalidate: 10
             }
         }
     )
+}
+
+async function getCommits () {
+    const data = await fetchGithub(`https://api.github.com/repos/${repoOwner}/${repoName}/commits`)
     return data.json()
 }
 
 async function getRepoInfo () {
-    const data = await fetch(
-        `https://api.github.com/repos/${repoOwner}/${repoName}`,
-        {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${githubToken}`
-            },
-            next: {
-                revalidate: 10
-            }
-        }
-    )
+    const data = await fetchGithub(`https://api.github.com/repos/${repoOwner}/${repoName}`)
     return data.json()
 }
 
